389ccccc132053d17856380d7a2d93306c36fd31
jaskaransingh9990 <jaskaransinghparmar.aie@gmail.com>
2024-04-02 10:48:01 -0700

    Making hero section

--- design-system.js
+++ design-system.js
@@ -8,3 +8,28 @@ document.addEventListener('DOMContentLoaded', function () {
         sideNavbar.classList.toggle('show');
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const carouselContainer = document.querySelector('.carousel-container');
+    const prevButton = document.querySelector('.prev');
+    const nextButton = document.querySelector('.next');
+  
+    let currentIndex = 0;
+    const numImages = carouselContainer.children.length;
+  
+    function moveCarousel(direction) {
+      const increment = direction === 'next' ? 1 : -1;
+      currentIndex = (currentIndex + increment + numImages) % numImages;
+      const newPosition = -currentIndex * 100;
+      carouselContainer.style.transform = `translateX(${newPosition}%)`;
+    }
+  
+    prevButton.addEventListener('click', function () {
+      moveCarousel('prev');
+    });
+  
+    nextButton.addEventListener('click', function () {
+      moveCarousel('next');
+    });
+  });
+  
